265725b2dec5aa064b4ea60bcfd3d1101a52a17c
Santiago Espindola <santiago2211ar@hotmail.com>
2024-03-19 19:27:11 -0300

    add functionality to modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,31 @@
 'use client';
+import { useEffect, useState } from 'react';
 import app from '../../firebaseConfig';
 import { FormContainer } from '../components/formPage/formContainer';
 import { Banner } from '../components/bannerHome/banner';
 import { AboutSection } from '../components/about/about';
 import { FAQsection } from '../components/FAQs/FAQs';
+import { Navbar } from '../components/navbar/navbar';
+import { Footer } from '../components/footer/footer';
+import { ModalUrls } from '@/components/myUrlsModal/modalUrls';
 
 export default function Home() {
   app;
+  const [links, setLinks] = useState<Array<[string, string]>>([]);
+
+  useEffect(() => {
+    const storedLinks = JSON.parse(localStorage.getItem("links") || "[]");
+    setLinks(storedLinks);
+  }, []);
   return (
      <main className="h-full">
+      <Navbar/>
+      <ModalUrls links={links}/>
       <Banner/>
       <FormContainer/> 
       <AboutSection/>
       <FAQsection/>
+      <Footer/>
      </main>
   )
   }
--- src/components/myUrlsModal/modalUrls.tsx
+++ src/components/myUrlsModal/modalUrls.tsx
@@ -1,4 +1,14 @@
-export function ModalUrls() {
+import { useState } from 'react';
+import copy from 'clipboard-copy';
+
+interface linksLs {
+  links: Array<[string, string]>;
+}
+
+export function ModalUrls({ links }: linksLs) {
+  const handleCopy = (shortUrl: string) => {
+    copy(`http://localhost:3000/${shortUrl}`);
+  };
   return (
     <section>
       <div
@@ -7,7 +17,7 @@ export function ModalUrls() {
         aria-hidden="true"
         className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
       >
-        <div className="relative p-4 w-full max-w-md max-h-full">
+        <div className="relative p-4 w-full max-w-[45rem] max-h-full">
           <div className="relative bg-white rounded-lg shadow ">
             <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t ">
               <h3 className="text-lg font-semibold text-gray-900 ">Connect wallet</h3>
@@ -25,12 +35,52 @@ export function ModalUrls() {
             <div className="p-4 md:p-5">
               <p className="text-sm font-normal text-gray-500 ">Connect with one of our available wallet providers or create a new one.</p>
               <ul className="my-4 space-y-3">
-                <li>
-                  <i className="fa-solid fa-share"></i>
-                  <i className="fa-solid fa-copy"></i>
-                  <p className=" font-bold">http://localhost:3000/tjg</p>
-                  <p className=" text-gray-700">https://www.hyperui.dev/components/application-ui/tables</p>
-                </li>
+                {links.map(([url, shortUrl], index) => (
+                  <li>
+                    <div className="relative rounded-lg border border-gray-200 shadow-lg" key={index}>
+                      <button className="absolute -end-1 -top-1 rounded-full border border-gray-300 bg-gray-100 p-1">
+                        <span className="sr-only">Close</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
+                          <path
+                            fillRule="evenodd"
+                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                            clipRule="evenodd"
+                          />
+                        </svg>
+                      </button>
+
+                      <div className="flex items-center gap-4 p-4">
+                        <img
+                          alt=""
+                          src="https://images.unsplash.com/photo-1611432579699-484f7990b127?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
+                          className="size-12 rounded-lg object-cover"
+                        />
+
+                        <div>
+                          <p className="font-medium text-gray-900">http://localhost:3000/{shortUrl}</p>
+
+                          <p className="line-clamp-1 text-sm text-gray-500">{url}</p>
+                          <div className="flex justify-center ">
+                            <a
+                              className="inline-block rounded-full border border-blue-600 bg-blue-600 p-3 text-white hover:bg-transparent hover:text-blue-600 focus:outline-none focus:ring active:text-blue-500 mx-1"
+                              href={`http://localhost:3000/${shortUrl}`}
+                              target="_blank"
+                            >
+                              <i className="fa-solid fa-share"></i>
+                            </a>
+                            <button
+                              className="inline-block rounded-full border border-blue-600 p-3 text-blue-600 hover:bg-blue-600 hover:text-white focus:outline-none focus:ring active:bg-blue-500 mx-1"
+                              onClick={() => handleCopy(shortUrl)}
+                            >
+                              <i className="fa-solid fa-copy"></i>
+                              <span className="px-1">COPY</span>
+                            </button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </li>
+                ))}
               </ul>
 
               <div>
