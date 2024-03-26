336a9ac1059e795eb3ca67dcd13bc4f3f4177ba9
Santiago Espindola <santiago2211ar@hotmail.com>
2024-03-26 20:19:06 -0300

    repair code for deploy to vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
       <body className={`${montserrat.className} antialiased`}>
         {children}
         
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
+        <script async src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
       </body>
     </html>
   );
