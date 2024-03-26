8f970c2de3d63afa71408a4c7a68407e995b222c
Santiago <santiago2211ar@hotmail.com>
2024-03-26 12:47:10 -0300

    repair [shortid] page

--- src/app/[shortId]/page.tsx
+++ src/app/[shortId]/page.tsx
@@ -2,36 +2,40 @@
 import { useParams } from 'next/navigation';
 import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
 import app from '../../../firebaseConfig';
+import { useEffect } from 'react';
 
-export default function shortIdPage() {
+export default function ShortIdPage() {
   app;
-  const params = useParams();
-  const shortId = params.shortId;
-  async function goLink(shortId: string | string[]): Promise<void> {
-    try {
-      const db = getFirestore();
-      const linksCollectionRef = collection(db, 'link');
-      const q = query(linksCollectionRef, where('shortUrl', '==', shortId));
+  useEffect(()=>{
+    const params = useParams();
 
-      const querySnapshot = await getDocs(q);
-
-      if (!querySnapshot.empty) {
-        querySnapshot.forEach((doc) => {
-          const linkData = doc.data();
+    const shortId = params.shortId;
+    async function goLink(shortId: string | string[]): Promise<void> {
+      try {
+        const db = getFirestore();
+        const linksCollectionRef = collection(db, 'link');
+        const q = query(linksCollectionRef, where('shortUrl', '==', shortId));
+        
+        const querySnapshot = await getDocs(q);
+        
+        if (!querySnapshot.empty) {
+          querySnapshot.forEach((doc) => {
+            const linkData = doc.data();
+            if (typeof window !== 'undefined') {
+              window.location.href = linkData.url;
+            }
+          });
+        } else {
           if (typeof window !== 'undefined') {
-            window.location.href = linkData.url;
+            window.location.href = '/';
           }
-        });
-      } else {
-        if (typeof window !== 'undefined') {
-          window.location.href = '/';
         }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
     }
-  }
-  goLink(shortId);
-
+    goLink(shortId);
+  },[])
+    
   return <p>Redirigiendo...</p>;
 }
